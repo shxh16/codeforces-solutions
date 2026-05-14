7faaa13b413e15d7867eed49fe8aeb4a93217efe
shxh16 <shrinil1612@gmail.com>
2026-05-14 18:58:57 +0530

    Create CYCLIC_MERGING.cpp

---
+++ 1300/CYCLIC_MERGING.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define Code ios::sync_with_stdio(false);
+#define by cin.tie(nullptr);
+#define Shrinil cout.tie(nullptr)
+const long long MOD = 1e9 + 7;
+ll ceil_div(ll a,ll b){
+    return (a+b-1LL)/(b);
+}
+bool pow_2(ll n){
+    if(n == 0) return true;
+    return ((n > 0)&&(n & (n - 1)) == 0);
+}
+ll n;
+void solve(){
+    cin >> n;
+    ll a[n];
+    ll answer = 0;
+    for(ll i = 0;i<n;++i){
+        cin >> a[i];
+        if(i){
+            answer += max(a[i],a[i-1]);
+        }
+    }
+    answer += max(a[n-1],a[0]);
+    answer -= *max_element(a,a+n);
+    cout << answer << '\n';
+}
+int main(){
+    Code by Shrinil;
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
